4c8727c2274b3c81999942fc7124c135244d8bd2
Whitney Harper <whitneyharper1@aol.com>
2022-01-02 17:39:37 -0500

    created new files index.html and styles.css in public folder. Setup socket connection in index.js and index.html

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const socket = require('socket.io');
 
 app.get('/', (req, res) => {
-
+    res.sendFile('public/index.html' , { root : __dirname})
 });
 
-app.listen(port, () => {
+//SERVER
+const server = app.listen(port, () => {
     console.log(`Server is running. Listening on ${port}`);
+});
+
+//SOCKET SETUP
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(`socket connection activated.`);
 })
